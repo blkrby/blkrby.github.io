8bc998cd5c1113cd9650ff371e9169d5599a1f8f
NoneForNowButMaybeLater <86891440+NoneForNowButMaybeLater@users.noreply.github.com>
2022-12-18 18:24:45 -0500

    foody push 2

--- projects/foody/general.js
+++ projects/foody/general.js
@@ -2,6 +2,11 @@
 const navigation = document.getElementById("nav");
 /*---------- Nav color changing variables start ----------*/
 
+/*Mobile nav varialbes start*/
+var mobileNavON = false;
+const mobileNav = document.getElementById("mobile_nav");
+/*---------- Mobile nav variables start ----------*/
+
 /*Nav color changing code start*/
 function changeNavBackground(){
     var scrollValue = window.scrollY;
@@ -14,3 +19,18 @@ function changeNavBackground(){
 }
 window.addEventListener('scroll', changeNavBackground);
 /*---------- Nav color changing code start ----------*/
+
+/*Mobile nav code start*/
+function toggleMobileNav(){
+    if(mobileNavON){
+        mobileNavON = false;
+        mobileNav.style.opacity = "0";
+        mobileNav.style.pointerEvents = "none";
+    }
+    else{
+        mobileNavON = true;
+        mobileNav.style.opacity = "1";
+        mobileNav.style.pointerEvents = "auto";
+    }
+}
+/*---------- Mobile nav code start ----------*/
