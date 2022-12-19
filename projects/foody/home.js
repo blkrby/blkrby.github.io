b0e45aadaa7c4cb1746e01d6376ea5b871510223
NoneForNowButMaybeLater <86891440+NoneForNowButMaybeLater@users.noreply.github.com>
2022-12-19 18:33:56 -0500

    foody push 4

--- projects/foody/home.js
+++ projects/foody/home.js
@@ -3,6 +3,12 @@ var commentsSliderMargin = 0;
 const comments = document.getElementById("comments_holder");
 /*---------- Comments slider variables start ----------*/
 
+/*Mobile comments slider variables*/
+var mobileCommentsSliderIndex = 0;
+var mobileCommentsSliderPosition = 0;
+const mobileComments = document.getElementById("mobile_comments_holder");
+/*---------- Mobile comments slider code variables ----------*/
+
 /*Comments slider code start*/
 function nextComment(){
     if(commentsSliderMargin < 1170)
@@ -19,3 +25,10 @@ function previousComment(){
     comments.style.marginLeft = -commentsSliderMargin +"px";
 }
 /*---------- Comments slider code start ----------*/
+
+/*Mobile comments slider code*/
+function setMobileSliderPosition(mobileCommentsSliderIndex){
+    mobileCommentsSliderPosition = 100 * mobileCommentsSliderIndex;
+    mobileComments.style.marginLeft = mobileCommentsSliderPosition + "%";
+}
+/*---------- Mobile comments slider code start ----------*/
