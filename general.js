64081505ffd3d50d1d86d6a9541eb2f0bfc6ccb9
NoneForNowButMaybeLater <86891440+NoneForNowButMaybeLater@users.noreply.github.com>
2023-02-26 14:27:29 -0500

    black ruby push 3

--- general.js
+++ general.js
@@ -7,6 +7,7 @@ let mobileNavMenuOn = false;
 const mobileNavButton = document.getElementById("mobile_nav_btn");
 const mobileNavButton2 = document.getElementById("mobile_nav_menu_btn");
 const mobileNavMenu = document.getElementById("mobile_nav_menu");
+const mobileNav = document.getElementById("mobile_nav");
 /*---------- mobile nav variables start ----------*/
 
 /*nav code start*/
@@ -16,15 +17,15 @@ function toggleNav(){
         nav.classList.add("scrolled_nav");
         nav.classList.remove("unscrolled_nav");
         
-        mobileNavMenu.classList.add("scrolled_nav");
-        mobileNavMenu.classList.remove("unscrolled_nav");
+        mobileNav.classList.add("scrolled_mobile_nav");
+        mobileNav.classList.remove("unscrolled_mobile_nav");
     }
     else{
         nav.classList.add("unscrolled_nav");
         nav.classList.remove("scrolled_nav");
         
-        mobileNavMenu.classList.add("unscrolled_nav");
-        mobileNavMenu.classList.remove("scrolled_nav");
+        mobileNav.classList.add("unscrolled_mobile_nav");
+        mobileNav.classList.remove("scrolled_mobile_nav");
     }
 }
 window.addEventListener('scroll', toggleNav);
